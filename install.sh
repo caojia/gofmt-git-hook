eecb5960d324157d4ab7bc1a6cdaf8727884cb5a
caojia <jerrycircle@gmail.com>
2017-03-21 00:35:39 +0800

    fix install script

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 go get golang.org/x/tools/cmd/goimports
 mkdir -p ~/bin
-curl -s https://raw.githubusercontent.com/caojia/gofmt-git-hook/master/go-fmt-check -o ~/bin 
-curl -s https://raw.githubusercontent.com/caojia/gofmt-git-hook/master/go-fmt-fix -o ~/bin 
+curl -s https://raw.githubusercontent.com/caojia/gofmt-git-hook/master/fmt-check -o ~/bin/go-fmt-check
+curl -s https://raw.githubusercontent.com/caojia/gofmt-git-hook/master/fmt-fix -o ~/bin/go-fmt-fix
 chmod +x ~/bin/go-fmt-check 
 chmod +x ~/bin/go-fmt-fix
 echo ~/bin/go-fmt-check > .git/hooks/pre-commit
